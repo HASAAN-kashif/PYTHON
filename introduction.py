7a319c75ef72bce4fdc98e355c7aff5fc4257db2
HASAAN-kashif <kyusenpai786@gmail.com>
2024-12-29 05:55:25 -0800

    v3

--- introduction.py
+++ introduction.py
@@ -1,2 +1,8 @@
 print("hello",end=" ")
 print('hello',2+3,5+9,sep="-")
+print('''my name is hasaan
+                        i study the following subjects
+                        1.maths
+                        2.science
+                        3.english
+                        ''')
