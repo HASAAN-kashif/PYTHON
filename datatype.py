bf289301413ec47154074ac64e24e346dfcbe57d
HASAAN-kashif <kyusenpai786@gmail.com>
2025-01-03 23:08:26 -0800

    v4

--- datatype.py
+++ datatype.py
@@ -4,3 +4,16 @@ b=2+5
 print('type of b:',type(b))
 c=2.5
 print('type of c:',type(c))
+name="penguin"
+age=14
+is_student=True
+weight=20
+#print different variable and their data type
+print("name:",name)
+print("data type of name is",type(name))
+print('age:',age)
+print("data type of age is",type(age))
+print('is_student:',is_student)
+print("data type of is_student is",type(is_student))
+print("weight:",weight)
+print("data type of weight is",type(weight))
