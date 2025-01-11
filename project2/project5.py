9cd4db282f388d130a8760439fdd6f1b9d482ffd
HASAAN-kashif <kyusenpai786@gmail.com>
2025-01-11 06:24:33 -0800

    v6

---
+++ project2/project5.py
@@ -0,0 +1,7 @@
+i=int(input('enter the temperature:'))
+if (i<20):
+    print('if i is smaller than 20')
+    print('it will be winter')
+else:
+    print('i is greater than 20')
+    print('it will be summer')
