4d58b2a95e080098a46622a445b20548247ad32e
HASAAN-kashif <kyusenpai786@gmail.com>
2025-01-25 04:09:23 -0700

    v1

---
+++ PROJECT1/project1.py
@@ -0,0 +1,20 @@
+a=12
+b=10
+c=0
+if a and b and c:
+     print('all the number has boolean value as true')
+else:
+     print('all the number has boolean value as false')
+
+a=10
+b=-10
+c=0
+if a>0 or b>0:
+     print("either the number is greater than 0")
+else:
+     print('no number is greater than 0')
+if b>0 or c>0:
+     print('either the number is greater than 0')
+else:
+     print('no number is greater than 0')
+
