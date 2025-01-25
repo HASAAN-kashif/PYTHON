84d32a04d2748ee18c311d4c036e4f6d202e20a0
HASAAN-kashif <kyusenpai786@gmail.com>
2025-01-25 04:20:03 -0700

    v2

--- PROJECT1/project1.py
+++ PROJECT1/project1.py
@@ -18,3 +18,19 @@ if b>0 or c>0:
 else:
      print('no number is greater than 0')
 
+a=10
+b=12
+c=12
+print(a!=b)
+print(b!=c)
+a="python"
+b="coding"
+if a!=b:
+     print(a,'and',b,'are different')
+a=4
+b=5
+if(a==1)!=(b==5):
+     print('hello')
+a=int(input('enter the number'))
+if a%2!=0:
+     print(a,'is not an even number,')
