2482af8f32f7f10b7b320ba44fc2d64e7ffbbaec
HASAAN-kashif <kyusenpai786@gmail.com>
2025-01-26 00:51:21 -0700

    v4

--- PROJECT1/project3.py
+++ PROJECT1/project3.py
@@ -4,3 +4,12 @@ if a.isalpha():
     print('a is an alphabet.')
 else:
     print('a is not an alphabet.')
+
+b = input("Enter a character: ")
+
+# Ensure the input is a single character
+if  a == 1:
+    ascii_code = ord(b)
+    print("The ASCII code of b.",ascii_code)
+else:
+    print("it is not an ASCII.")
