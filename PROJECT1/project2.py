85c39f5f51aad74c7807ae8edbe82065c0629fd8
HASAAN-kashif <kyusenpai786@gmail.com>
2025-01-26 00:03:09 -0700

    v2

--- PROJECT1/project2.py
+++ PROJECT1/project2.py
@@ -15,3 +15,14 @@ if (x is y):
 Y=30
 if (x is not y):
     print('x & y have different identity')
+
+a=10
+b=-10
+#print bitwise right shift operator
+print('a << 1=',a << 1)
+print('b << 1=',b << 1)
+a=5
+b=-10
+# print bitwise left shift operator
+print('a << 1=',a << 1)
+print('b << 1=',b << 1)
