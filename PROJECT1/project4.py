94af7b9758f7a010bf4eca7859420a3fa5aa027e
HASAAN-kashif <kyusenpai786@gmail.com>
2025-02-01 05:05:08 -0700

    v2

---
+++ PROJECT1/project4.py
@@ -0,0 +1,14 @@
+v=4
+w=3
+x=2
+y=1
+z=0
+z=(v+w)*x/y;
+print('value of (v+w)*x/y is',z)
+
+name='alex'
+age=0
+if name== 'alex' or name== 'john' and age>=2:
+    print('hello! world')
+else:
+    print('good bye!')
