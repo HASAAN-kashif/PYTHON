92ffe9bafbd24403c78626cd6d8f28dcdeffb34e
HASAAN-kashif <kyusenpai786@gmail.com>
2025-02-01 04:56:41 -0700

    v1

--- PROJECT1/project3.py
+++ PROJECT1/project3.py
@@ -7,9 +7,6 @@ else:
 
 b = input("Enter a character: ")
 
-# Ensure the input is a single character
-if  a == 1:
-    ascii_code = ord(b)
-    print("The ASCII code of b.",ascii_code)
-else:
-    print("it is not an ASCII.")
+
+a= input('enter the character')
+print(ord(a))
