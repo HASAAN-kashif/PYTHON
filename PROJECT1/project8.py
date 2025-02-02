932b77eb04b0479fd8c2171cbddd107bdf314bbf
HASAAN-kashif <kyusenpai786@gmail.com>
2025-02-02 00:32:21 -0700

    v5

--- PROJECT1/project8.py
+++ PROJECT1/project8.py
@@ -4,3 +4,9 @@ c = 3
 print("Before swapping:", a, b, c)
 a, b, c = c,b, a
 print("After swapping:", a, b, c)
+
+age=int(input('enter your age'))
+if 10>=age<=20:
+    print('you are allowed to enroll')
+else:
+    print('you are not allowed to enroll')
