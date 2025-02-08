091f05d6751db764850f49c88e3888194cf2407a
HASAAN-kashif <kyusenpai786@gmail.com>
2025-02-08 07:55:56 -0700

    v4

--- PROJECT1/project11.py
+++ PROJECT1/project11.py
@@ -2,3 +2,9 @@ n=int(input('enter the value of n:'))
 print('number from {0} to {1} are'.format(n,1))
 for i in range(n,0,-1):
     print(i)
+
+msg=input('enter the message')
+count=0
+for c in msg:
+    count=count+1
+print('count=',count)
