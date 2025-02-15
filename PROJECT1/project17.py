951cbd02458561ebf1ba34a23994304167e4c69a
HASAAN-kashif <kyusenpai786@gmail.com>
2025-02-15 00:43:49 -0700

    v2

--- PROJECT1/project17.py
+++ PROJECT1/project17.py
@@ -7,3 +7,14 @@ while(i<len(string)):
     count=count+1
  i=i+1
 print('the total number of times',char,'has occured',count)
+
+lower=int(input('enter a upper range:'))
+upper=int(input('enter a lower range'))
+print('prime number between',lower,"and",upper,"are:")
+for num in range(lower,upper+1):
+  if num>1:
+    for i in range(2,num):
+      if(num%i)==0:
+        break
+    else:
+      print(num)
