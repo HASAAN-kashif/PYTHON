ac9c8c8181d00f316740f02bce18aff69df4a736
HASAAN-kashif <kyusenpai786@gmail.com>
2025-02-22 03:49:01 -0700

    v2

--- PROJECT1/project21.py
+++ PROJECT1/project21.py
@@ -4,3 +4,12 @@ for i in range(n):
     for j in range(i+1):
         print('*',end="")
     print()
+
+rows=int(input('please enter the total number of rows'))
+number=1
+print('floyd triangle')
+for i in range(1,rows+1):
+    for j in range(1,i+1):
+        print(number,end='')
+        number=number+1
+    print()
