d30160b76b85d4834c57a588aedd50d064fc94d7
HASAAN-kashif <kyusenpai786@gmail.com>
2025-02-23 00:55:47 -0700

    v4

---
+++ PROJECT1/project25.py
@@ -0,0 +1,9 @@
+import turtle
+turtle.Screen().bgcolor('light blue')
+turtle= turtle.Turtle()
+turtle.pensize(3)
+turtle.pencolor("darkblue")
+for _ in range(4):
+    turtle.forward(100)
+    turtle.right(90)
+turtle.pendown()
